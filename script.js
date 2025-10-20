16062521133b0353a4d077dbd6a8b31aaa0a1904
colts115 <coltstrahan115@gmail.com>
2025-10-19 22:54:42 -0400

    created favoriteFoods array, looped through the list and printed favorite foods, printed out the rating for each food, created food reccomendation function

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-console.log("Hello World!");
+
 // 1. Create an array called favoriteFoods with at least 6 foods you love.
+
+const fav_foods = ["steak", "cereal", "doughnuts", "pizza", "chicken", "tacos"];
 
 
 
 // 2. Loop through the list and print: "One of my favorite foods is ______."
 
-
-
+for (let i = 0; i < fav_foods.length; i++){
+  console.log("One of my favorite foods is", fav_foods[i]);
+}
 
 
 // 3. Print out the rating for each food with a ranking like:
 // "My #1 favorite food is Ramen" (copy/paste for all items)
 // "My #2 favorite food is Sushi"
 // ...etc.
+for (let i = 0; i < fav_foods.length; i++){
+  console.log('my #' + [i+1] + ' favorite food is ' + fav_foods[i]);
+}
 
 
 
@@ -23,37 +29,15 @@ console.log("Hello World!");
     // "I always recommend ____ to friends."
     // "Trust me — ____ is delicious."
 
+function printFoodRecommendation(foodname){
+  console.log("Have you ever tried " + foodname+ "?");
+  console.log("I always reccomend " + foodname + " to friends.");
+  console.log("Trust me, " + foodname + " is delicious.");
+  
+
+}
 
 
 
 // 4b. Call the function at least 3 times
 
-
-
-// Here's a list of 50 friends' favorite foods:
-let friendFavorites = [
-    "Pizza", "Sushi", "Pasta", "Falafel", "Burgers", "Ramen", "Pad Thai", "Curry", "Pho", "Nachos", "Gnocchi", "Donuts", "Steak", "Lasagna", "Biryani", "Tacos", "Croissant", "Churros", "Fried Rice", "Shawarma", "Miso Soup", "BBQ Ribs", "Hotpot", "Enchiladas", "Baklava", "Gyros", "Hummus", "Empanadas", "Pancakes", "Muffins", "Samosas", "Macarons", "Quiche", "Pierogi", "Arepas", "Okonomiyaki", "Ceviche", "Brisket", "Bao Buns", "Poutine", "Clam Chowder", "Fajitas", "Canelé", "Kimchi", "Tamales", "Omelette", "Biscuits", "Tempura", "Spring Rolls", "Crepes"
-  ];
-
-// 5. Print out only foods that have an "a" in the name. For example, "Pizza" would not be included, but "Pasta" would be.
-
-
-
-// 6. Store the result in an array called foodsWithA. Print out the array.
-
-
-
-// 7. Create a new array longFoodNames for foods with names longer than 6 characters.
-
-
-
-// 8. Create another array shortFoodNames for foods 6 characters or shorter.
-
-
-
-// 9. Print both arrays and compare:
-// "There are more long-named foods." OR "There are more short-named foods."
-
-
-// 10. STRETCH: Find the longest food name and print:
-// "The longest food name in the list is ______ with ___ characters."
